d6488936c1765c7339b4e9f40e12fd37b242f508
bozidar <bozidar.labas@hotmail.com>
2015-12-11 12:57:14 +0100

    Implemented change security Action Button

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -10,7 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-
+    
+    @IBOutlet weak var etUsername: UITextField!
+    
+    @IBOutlet weak var etPassword: UITextField!
+    
+    @IBOutlet weak var tvUsername: UILabel!
+    
+    @IBOutlet weak var tvPassword: UILabel!
+    
+    //This is called when view is loaded (outlets are already initialized)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var secure: Bool = false{
+        didSet{
+            updateUI()
+        }
+    }
+    
+    private func updateUI(){
+        etPassword.secureTextEntry = secure
+        tvPassword.text = secure ? "Secured password" : "Password"
+    }
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
+    
 }
 
