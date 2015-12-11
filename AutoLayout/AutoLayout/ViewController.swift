77452b8c08bc6931885210724888c15ee7600aee
bozidar <bozidar.labas@hotmail.com>
2015-12-11 11:07:08 +0100

    Initial AutoLayout project setup

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Bozidar on 11.12.2015..
+//  Copyright © 2015. Bozidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
