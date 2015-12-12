14dc11d27f65d8d653e6bec77fad80d341e7376d
bozidar <bozidar.labas@hotmail.com>
2015-12-12 15:29:04 +0100

    Added search functionality for tweet

---
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Bozidar on 12.12.2015..
+//  Copyright © 2015. Bozidar. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+}
--- Smashtag/Smashtag/TweetTableViewController.swift
+++ Smashtag/Smashtag/TweetTableViewController.swift
@@ -8,27 +8,58 @@
 
 import UIKit
 
-class TweetTableViewController: UITableViewController {
+class TweetTableViewController: UITableViewController, UITextFieldDelegate {
     
     //Array of array
     var tweets = [[Tweet]]()
-
-    //Here is fetced model data
     
+    var searchText: String? = "#standford"{
+        didSet{
+            etSearch?.text = searchText
+            //clear table and refresh
+            tweets.removeAll()
+            tableView.reloadData()
+            refresh()
+        }
+    }
+
+    @IBOutlet weak var etSearch: UITextField!{
+        didSet{
+            etSearch.delegate = self
+            etSearch.text = searchText
+        }
+    }
+    
+    func refresh(){
+        if searchText != nil{
+            let request = TwitterRequest(search: searchText!, count: 100)
+            request.fetchTweets { ( newTweets ) -> Void in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    //Code when tweets are fetched (this block of code is in main thread)
+                    if newTweets.count > 0 {
+                        self.tweets.insert(newTweets, atIndex: 0)
+                        self.tableView.reloadData()
+                    }
+                })
+                
+            }
+        }
+    }
+    
+    //When someone click return
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == etSearch{
+            textField.resignFirstResponder() //dismis keyboard
+            searchText = textField.text
+        }
+        return true
+    }
+    
+    //Here is fetced model data
     //MARK: View Controller LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let request = TwitterRequest(search: "#standford", count: 100)
-        request.fetchTweets { ( newTweets ) -> Void in
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                //Code when tweets are fetched (this block of code is in main thread)
-                if newTweets.count > 0 {
-                    self.tweets.insert(newTweets, atIndex: 0)
-                    self.tableView.reloadData()
-                }
-            })
-            
-        }
+        refresh()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
